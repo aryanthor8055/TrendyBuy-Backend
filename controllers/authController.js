6df42c52ba0b201eb4355dcf96e98c6f7f26fa4a
Aryan <aryan16072001236809@gmail.com>
2023-05-21 16:32:38 +0530

    Adding private routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,26 +9,26 @@ export const registerController = async (req, res) => {
 
     //Validations
     if (!name) {
-      return res.send({ error: "Name is Required" });
+      return res.send({ message: "Name is Required" });
     }
     if (!email) {
-      return res.send({ error: "Email is Required" });
+      return res.send({ message: "Email is Required" });
     }
     if (!password) {
-      return res.send({ error: "Password is Required" });
+      return res.send({ message: "Password is Required" });
     }
     if (!phone) {
-      return res.send({ error: "Phone is Required" });
+      return res.send({ message: "Phone is Required" });
     }
     if (!address) {
-      return res.send({ error: "Address is Required" });
+      return res.send({ message: "Address is Required" });
     }
     //Check user
     const existinguser = await userModel.findOne({ email });
     //existing user
     if (existinguser) {
       return res.status(200).send({
-        success: true,
+        success: false,
         message: "Already Register please login",
       });
     }
