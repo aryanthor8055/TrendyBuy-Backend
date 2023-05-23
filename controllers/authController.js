9b588e0b348396a7ab05e99cd621dd58a8963a52
Aryan <aryan16072001236809@gmail.com>
2023-05-23 15:55:04 +0530

    Added Forgot Password Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ import JWT from "jsonwebtoken";
 // REGISTER
 export const registerController = async (req, res) => {
   try {
-    const { name, email, password, phone, address } = req.body;
+    const { name, email, password, phone, address, answer } = req.body;
 
     //Validations
     if (!name) {
@@ -22,6 +22,9 @@ export const registerController = async (req, res) => {
     }
     if (!address) {
       return res.send({ message: "Address is Required" });
+    }
+    if (!answer) {
+      return res.send({ message: "Answer is Required" });
     }
     //Check user
     const existinguser = await userModel.findOne({ email });
@@ -41,6 +44,7 @@ export const registerController = async (req, res) => {
       phone,
       address,
       password: hashedPassword,
+      answer,
     }).save();
 
     res.status(201).send({
@@ -106,6 +110,54 @@ export const loginController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error in login",
+      error,
+    });
+  }
+};
+
+//forgotPasswordController
+
+export const forgotPasswordController = async (req, res) => {
+  try {
+    const { email, answer, newPassword } = req.body;
+
+    if (!email) {
+      res.status(400).send({
+        message: "Email is required",
+      });
+    }
+    if (!answer) {
+      res.status(400).send({
+        message: "Answer is required",
+      });
+    }
+    if (!newPassword) {
+      res.status(400).send({
+        message: "New Password is required",
+      });
+    }
+
+    //check
+    const user = await userModel.findOne({ email, answer });
+
+    //Validation
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Wrong Email or Answer",
+      });
+    }
+    const hashed = await hashPassword(newPassword);
+    await userModel.findByIdAndUpdate(user._id, { password: hashed });
+    res.status(200).send({
+      success: true,
+      message: "Password Reset Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Something went Wrong",
       error,
     });
   }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,6 +3,7 @@ import {
   registerController,
   loginController,
   testController,
+  forgotPasswordController,
 } from "../controllers/authController.js";
 
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
@@ -17,6 +18,10 @@ router.post("/register", registerController);
 
 //LOGIN||POST
 router.post("/login", loginController);
+
+//Forgot Password
+
+router.post("/forgot-password", forgotPasswordController);
 
 //test routes
 router.get("/test", requireSignIn, isAdmin, testController);
