09a1db8fbf17352f3a61fe435f139113a01e1343
AryanflexC <140161962+AryanflexC@users.noreply.github.com>
2023-07-23 21:40:38 +0530

    created getProducts and getSingleProduct Controllers and routes, next---working on getphoto route

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 import slugify from "slugify";
 import productModel from "../models/productModel.js";
 import fs from "fs";
+import { log } from "console";
 
 export const createProductController = async (req, res) => {
   try {
@@ -12,7 +13,7 @@ export const createProductController = async (req, res) => {
     switch (true) {
       case !name:
         return res.status(500).send({ error: "Name is required" });
-     
+
       case !description:
         return res.status(500).send({ error: "Description is required" });
       case !price:
@@ -21,29 +22,78 @@ export const createProductController = async (req, res) => {
         return res.status(500).send({ error: "Category is required" });
       case !quantity:
         return res.status(500).send({ error: "Quantity is required" });
-        case photo && photo.size > 100000:
-            return res.status(500).send({error:'photo is required and should be less than 1mb'})
-      
+      case photo && photo.size > 100000:
+        return res
+          .status(500)
+          .send({ error: "photo is required and should be less than 1mb" });
     }
 
-    const products = new productModel({...req.fields,slug:slugify(name)})
-    if(photo){
-        products.photo.data=fs.readFileSync(photo.path)
-        products.photo.contentType=photo.type
+    const products = new productModel({ ...req.fields, slug: slugify(name) });
+    if (photo) {
+      products.photo.data = fs.readFileSync(photo.path);
+      products.photo.contentType = photo.type;
     }
 
-    await products.save()
+    await products.save();
     res.status(201).send({
-        success:true,
-        message:'Product Created Successfully',
-        products,
-    })
+      success: true,
+      message: "Product Created Successfully",
+      products,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
       error,
       message: "Error in creating product",
+    });
+  }
+};
+
+//get all products
+export const getProductController = async (req, res) => {
+  try {
+    const products = await productModel
+      .find({})
+      .populate("category")
+      .select("-photo")
+      .limit(12)
+      .sort({ createdAt: -1 });
+    res.status(200).send({
+      success: true,
+      Totalcount: products.length,
+      message: "AllProducts",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in getting Products",
+      error: error.message,
+    });
+  }
+};
+
+//get Single Product
+
+export const getSingleProductController = async (req, res) => {
+  try {
+    const product = await productModel
+      .findOne({ slug: req.params.slug })
+      .select("-photo")
+      .populate("category");
+    res.status(200).send({  
+      success: true,
+      message: "Single Product Fetched",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in getting Product",
+      error,
     });
   }
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,17 +1,30 @@
-import express from 'express'
-import {isAdmin, requireSignIn} from '../middlewares/authMiddleware.js'
-import { createProductController } from '../controllers/productController.js'
-import formidable from 'express-formidable'
+import express from "express";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import {
+  createProductController,
+  getProductController,
+  getSingleProductController,
+} from "../controllers/productController.js";
+import formidable from "express-formidable";
 
-
-const router=express.Router()
+const router = express.Router();
 
 //routes
 
-router.post('/create-product',requireSignIn,isAdmin,formidable(),createProductController)
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
 
+//get Product
+router.get("/get-product", getProductController);
 
-//getProduct
+//single product
+router.get("/get-product/:slug", getSingleProductController);
 
+//get photo
 
 export default router;
