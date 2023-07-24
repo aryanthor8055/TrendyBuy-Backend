2651aad5ac9bd599c7bdc2d613030aecf8379fb9
aryanthor8055 <aryan16072001236809@gmail.com>
2023-07-24 11:08:26 +0530

    Revert "created getProducts and getSingleProduct Controllers and routes, next---working on getphoto route"
    This reverts commit 09a1db8fbf17352f3a61fe435f139113a01e1343.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,30 +1,17 @@
-import express from "express";
-import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import {
-  createProductController,
-  getProductController,
-  getSingleProductController,
-} from "../controllers/productController.js";
-import formidable from "express-formidable";
+import express from 'express'
+import {isAdmin, requireSignIn} from '../middlewares/authMiddleware.js'
+import { createProductController } from '../controllers/productController.js'
+import formidable from 'express-formidable'
 
-const router = express.Router();
+
+const router=express.Router()
 
 //routes
 
-router.post(
-  "/create-product",
-  requireSignIn,
-  isAdmin,
-  formidable(),
-  createProductController
-);
+router.post('/create-product',requireSignIn,isAdmin,formidable(),createProductController)
 
-//get Product
-router.get("/get-product", getProductController);
 
-//single product
-router.get("/get-product/:slug", getSingleProductController);
+//getProduct
 
-//get photo
 
 export default router;
